e709a648a7fc1f2697d7d0a16ea381d36ad8837b
Raul Valdez <rd.valdez@outlook.com>
2024-04-17 11:02:17 -0500

    Added some model classes for the database.

--- app/models/models.py
+++ app/models/models.py
@@ -8,18 +8,50 @@ class Base(DeclarativeBase):
 
 
 class Campus(Base):
-    '''Campus model to store campus information.'''
+    '''Campus model to store campus information.
+    id: int - primary key
+    campus_name: str - campus name: Ex. 'University of Texas'
+    campus_abbreviation: str - campus abbreviation: Ex. 'UT'
+    location: Location - location of the campus. 
+    '''
     __tablename__ = 'campuses'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     campus_name: Mapped[str] = mapped_column('Campus Name', String, nullable=False)
     campus_abbreviation: Mapped[str] = mapped_column('Campus Abbreviation', String, nullable=False)
+    location = relationship('Location', backref='campus')
 
     def __repr__(self) -> str:
         return f'{self.campus_name}'
 
 
+class Location(Base):
+    '''Location model to store location information.
+    id: int - primary key
+    '''
+    __tablename__ = 'locations'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    city: Mapped[str] = mapped_column('City', String, nullable=False)
+    state: Mapped[str] = mapped_column('State', String, nullable=False)
+    state_abbreviation: Mapped[str] = mapped_column('State Abbreviation', String, nullable=False)
+    zip_code: Mapped[str] = mapped_column('Zip Code', String, nullable=False)
+    campus_id: Mapped[int] = mapped_column('Campus ID', Integer, ForeignKey('campuses.id'), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'{self.city}, {self.state}'
+
+
 class Medication(Base):
-    '''Medication model to store medication information.'''
+    '''Medication model to store medication information.
+    id: int - primary key
+    generic_name: str - generic name of the medication: Ex. 'acetaminophen'
+    brand_name: str - brand name of the medication: Ex. 'Tylenol'
+    strength: int - strength of the medication: Ex. 500
+    unit: str - unit of the medication: Ex. 'mg'
+    form: str - form of the medication: Ex. 'tablet', 'capsule'
+    barcode: str - barcode of the medication: Ex. '0123456789'
+
+    '''
     __tablename__ = 'medications'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -59,6 +91,12 @@ class Vendor(Base):
     vendor_contact: Mapped[str] = mapped_column('Vendor Contact', String, nullable=False)
     vendor_phone: Mapped[str] = mapped_column('Vendor Phone', String, nullable=False)
     vendor_email: Mapped[str] = mapped_column('Vendor Email', String, nullable=False)
+    medication = relationship('Medication', backref='vendor')
 
     def __repr__(self) -> str:
         return f'{self.vendor_name}'
+
+# Create the tables in the database.
+Base.metadata.drop_all(session.get_bind())
+Base.metadata.create_all(session.get_bind())
+
